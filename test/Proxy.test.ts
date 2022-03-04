5b3c89554984e5fd32cc5702bdba212756447bb0
kevin <aerockevin@gmail.com>
2022-03-04 16:47:39 +0800

    proxy getContractAt implementation

--- test/Proxy.test.ts
+++ test/Proxy.test.ts
@@ -1,29 +1,30 @@
 import {setup} from './utils';
 import {expect} from "chai";
 import {ethers} from "hardhat";
-
+import {V1} from "../typechain";
 
 
 describe('Proxy', () => {
 
     it("check proxy", async () => {
         const {owner} = await setup();
-        
+
         const implementation = await owner.Proxy.implementation();
         expect(implementation).to.eq(owner.V1.address);
-        
+
         const calldata = owner.V1.interface.encodeFunctionData("inc");
-        
+
+        const v1Proxy = await ethers.getContractAt<V1>("V1", owner.Proxy.address);
+        expect(await v1Proxy.x()).to.eq(0);
+
         const signer = await ethers.getSigner(owner.address);
-        const res = await signer.sendTransaction({
+        await signer.sendTransaction({
             to: owner.Proxy.address,
             value: 0,
             data: ethers.utils.hexlify(calldata)
         });
-
-        // console.log(res);
-        // TODO: how to view through proxy?
-
+        expect(await v1Proxy.x()).to.eq(1);
+        
     });
 
 
